93a224a1196120131f1028c7057f0077f547abf9
tobimai <me@maiertobi.de>
2020-02-01 12:14:45 -0300

    added scripts

---
+++ scripts/docker-run.sh
@@ -0,0 +1,1 @@
+nvidia-docker run -ti --rm -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/developer/.Xauthority -v /home/$USER:/home/$USER --net=host --pid=host --ipc=host tano297/bonnetal:runtime /bin/bash
