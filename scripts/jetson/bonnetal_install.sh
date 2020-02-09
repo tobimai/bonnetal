6f71695da99d69b78c2b7aa88f76cc3b69109767
tobimai <me@maiertobi.de>
2020-02-09 13:01:55 -0300

    added jetson scripts

---
+++ scripts/jetson/bonnetal_install.sh
@@ -0,0 +1,31 @@
+sudo apt install python-empy
+sudo pip install catkin_tools trollius
+
+echo "Installing Pytorch now, this could take a while"
+sleep 3
+
+sudo swapoff /swapfile
+sudo dd if=/dev/zero of=/swapfile bs=1M count=8192 oflag=append conv=notrunc
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+
+
+git clone --recursive https://github.com/tano297/pytorch
+cd pytorch
+export USE_NCCL=0
+export USE_DISTRIBUTED=0
+export TORCH_CUDA_ARCH_LIST="5.3;6.2;7.2"
+export MAX_JOBS=3
+sudo -E python3 setup.py install
+sudo cp -r torch/include/* /usr/local/include
+sudo cp -r torch/lib/* /usr/local/lib
+sudo cp -r torch/share/* /usr/local/share
+
+echo "finished building PyTorch, building bonnetal now"
+
+cd ../bonnetal/deploy
+git clone https://github.com/ros/catkin.git src/catkin
+CMAKE_PREFIX_PATH=""
+catkin init
+catkin build bonnetal_segmentation_standalone bonnetal_segmentation_ros
---
+++ scripts/jetson/setup.sh
@@ -0,0 +1,17 @@
+sudo gsettings set org.gnome.desktop.session idle-delay 0
+sudo apt update
+sudo apt upgrade -y
+sudo apt install tmux git python3 python3-pip htop
+
+sudo apt autoremove -y
+
+sudo apt-get install -yqq  build-essential ninja-build \
+  python3-dev python3-pip apt-utils curl git cmake unzip autoconf autogen \
+  libtool mlocate zlib1g-dev python3-numpy python3-wheel wget \
+  software-properties-common openjdk-8-jdk libpng-dev  \
+  libxft-dev ffmpeg libboost-all-dev \
+  libyaml-cpp-dev
+
+sudo updatedb
+
+echo "Install ROS by running the ros_install.sh as root"
