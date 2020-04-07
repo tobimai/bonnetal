600585a6ceaf4de33ef2185c914daffb473bcd7c
tobimai <me@maiertobi.de>
2020-04-07 07:44:21 +0200

    updated script

--- scripts/jetson/bonnetal_install.sh
+++ scripts/jetson/bonnetal_install.sh
@@ -1,9 +1,13 @@
-sudo apt install python-empy
+sudo apt update
+sudo apt-get install -y build-essential ninja-build python3-dev python3-pip apt-utils curl git cmake unzip autoconf autogen libtool mlocate zlib1g-dev python3-numpy python3-wheel wget software-properties-common openjdk-8-jdk libpng-dev libxft-dev ffmpeg libboost-all-dev libyaml-cpp-dev
+sudo apt install python-empy python-catkin-tools
 sudo pip install catkin_tools trollius
+sudo updatedb
 
 echo "Installing Pytorch now, this could take a while"
 sleep 3
 
+echo "Creating 8GB Swap"
 sudo swapoff -a
 sudo dd if=/dev/zero of=/swapfile bs=1M count=8192 oflag=append conv=notrunc
 sudo mkswap /swapfile
